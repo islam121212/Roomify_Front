49d463a6d5520133b66fb15967a4eec6e2d7f90f
Kimo masr <eslamembaby33@gmail.com>
2025-03-09 04:40:20 +0200

    make login / register

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+@Component({
+  selector: 'app-login',
+  imports: [RouterModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
---
+++ src/app/pages/register/register.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators , ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+  imports: [CommonModule, ReactiveFormsModule , RouterModule], 
+  standalone:true,
+})
+export class RegisterComponent {
+  registerForm: FormGroup;
+  submitted = false;
+
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      fullName: ['', [Validators.required, Validators.minLength(3)]],
+      username: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9_]+$')]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.pattern('(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}')]]
+    });
+  }
+
+  get f() {
+    return this.registerForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.registerForm.valid) {
+      console.log('Registration Successful', this.registerForm.value);
+      // يمكنك إرسال البيانات إلى الـ API هنا
+    }
+  }
+}
