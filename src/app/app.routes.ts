cc4a5d44c5b3f777b38703fcc2ff2806bc563945
Kimo masr <eslamembaby33@gmail.com>
2025-03-11 21:40:10 +0200

    generate page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import { GenerateComponent } from './pages/generate/generate.component';
 
 export const routes: Routes = [
     { path: 'register', component: RegisterComponent },
     { path: 'login', component: LoginComponent },
     { path: 'home', component: HomeComponent},
+    { path: 'generate', component: GenerateComponent},
     { path: '', redirectTo: '/register', pathMatch: 'full' }
 ];
---
+++ src/app/pages/generate/generate.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generate',
+  imports: [],
+  templateUrl: './generate.component.html',
+  styleUrl: './generate.component.css'
+})
+export class GenerateComponent {
+
+}
