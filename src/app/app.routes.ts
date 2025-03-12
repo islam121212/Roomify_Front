cde6562f5c171e2401b66e86479f5a24c2238f71
Kimo masr <eslamembaby33@gmail.com>
2025-03-12 19:43:20 +0200

    profile page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { GenerateComponent } from './pages/generate/generate.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 export const routes: Routes = [
     { path: 'register', component: RegisterComponent },
     { path: 'login', component: LoginComponent },
     { path: 'home', component: HomeComponent},
     { path: 'generate', component: GenerateComponent},
+    { path: 'profile', component: ProfileComponent},
     { path: '', redirectTo: '/register', pathMatch: 'full' }
 ];
