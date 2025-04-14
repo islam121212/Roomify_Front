efdf688691a3f1fa4fa3dc108fec50a392c99273
islam121212 <eslamembaby33@gmail.com>
2025-04-14 15:29:59 +0200

    add change page

--- src/app/change-password/change-password.component.ts
+++ src/app/change-password/change-password.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-change-password',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule , CommonModule],
   templateUrl: './change-password.component.html',
-  styleUrl: './change-password.component.css'
+  styleUrls: ['./change-password.component.css']
 })
 export class ChangePasswordComponent {
+  showModal = true;
 
+  oldPassword = '';
+  newPassword = '';
+
+  openModal() {
+    this.showModal = true;
+  }
+
+  closeModal() {
+    this.showModal = false;
+  }
+
+  confirmChange() {
+    console.log('Old:', this.oldPassword);
+    console.log('New:', this.newPassword);
+    this.closeModal();
+  }
 }
--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-settings',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule], // ✅ صح
   templateUrl: './settings.component.html',
-  styleUrl: './settings.component.css'
+  styleUrls: ['./settings.component.css'] // ✅ جمع مش مفرد
 })
 export class SettingsComponent {
 
+  constructor(private router: Router) {} // ✅ لازم الـ constructor
+
+  goToSettings() {
+    this.router.navigate(['/changepass']);
+  }
 }
